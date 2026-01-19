16a7d9bd5b84da2022a48d953c60fdd537a7b1bb
ssoj13 <96214515+ssoj13@users.noreply.github.com>
2026-01-19 11:00:52 -0800

    chore: WIP Mon 01/19/2026 - 11:00:52.50

--- src/viewer/renderer/postfx.rs
+++ src/viewer/renderer/postfx.rs
@@ -222,7 +222,7 @@ pub fn create_postfx_pipelines(
                 ty: wgpu::BindingType::Texture {
                     multisampled: false,
                     view_dimension: wgpu::TextureViewDimension::D2,
-                    sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                    sample_type: wgpu::TextureSampleType::Float { filterable: true },
                 },
                 count: None,
             },
